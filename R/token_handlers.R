fd37d3fb2d363a2a979910cba5057d85fe53fd36
Trevor Burrows <dburrows@purdue.edu>
2021-12-29 20:38:03 -0500

    Beginning readme and function documentation

--- R/token_handlers.R
+++ R/token_handlers.R
@@ -1,11 +1,31 @@
-set_api_key <- function(key = NULL) {
+#' Set NOTION_KEY as an environment variable
+#'
+#' Call \code{set_notion_key()} to save your Notion secret key as an environment variable. Leave the \code{key} parameter empty to call \code{askpass} and enter your secret there.
+#'
+#' @param key A string.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{set_notion_key()}
+set_notion_key <- function(key = NULL) {
   if (is.null(key)) {
     key <- askpass::askpass("Please enter your Notion API Key")
   }
   Sys.setenv("NOTION_KEY" = key)
 }
 
-get_api_key <- function() {
+#' Get and return NOTION_KEY
+#'
+#' Returns the NOTION_KEY environment variable. If no key is set, the function will return a warning.
+#'
+#' @return NOTION_KEY as a string
+#' @export
+#'
+#' @examples
+#' \dontrun{get_notion_key()}
+get_notion_key <- function() {
   key <- Sys.getenv("NOTION_KEY")
   if (identical(key, "")) {
     stop("No API key found, please supply with `NOTION_KEY` argument or use `set_api_key` to set the NOTION_KEY environment variable")
